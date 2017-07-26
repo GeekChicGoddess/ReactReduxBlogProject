fb60c4bdd749194a6e0cdce8bc01708688bba72f
melodytempleton <mtempletoncantu@gmail.com>
2017-07-26 16:12:02 -0500

    Add posts reducer

---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,14 @@
+/**
+ * Created by melodytempleton on 7/26/17.
+ */
+import { FETCH_POSTS } from '../actions';
+import _ from 'lodash';
+
+export default function (state = {}, action) {
+    switch (action.type){
+        case FETCH_POSTS:
+            return _.mapKeys(action.payload.data, 'id');
+        default:
+            return state;
+    }
+}
