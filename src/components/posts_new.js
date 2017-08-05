329015c466dc810fd5444cf95ed8ead740166f80
melodytempleton <mtempletoncantu@gmail.com>
2017-08-05 12:51:00 -0500

    Add PostsNew component and route

---
+++ src/components/posts_new.js
@@ -0,0 +1,19 @@
+/**
+ * Created by melodytempleton on 8/5/17.
+ */
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+
+    render() {
+        return(
+            <div>
+                Posts new
+            </div>
+
+        );
+    }
+
+}
+
+export default PostsNew;
